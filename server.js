cacbfa8750c8bf95fe028a640778aa2456b263fc
Adewunmi0303 <adewunmiade02@gmail.com>
2025-06-01 19:52:12 +0100

    Week 13

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 
+import { name } from 'ejs';
 import express from 'express'
 
 const app = express()
 
 const PORT = 3000
 
-// Define a simple route
-app.get('/', (req,res)=>{
-    res.send('Hello, Express!')
-})
+
+app.get('/user/:id', (req,res)=>{
+    const userid = req.params.id;
+    res.send(`User ID is ${userid}`);
+});
+
+// /search?keyword=node
+app.get('/search',(req,res)=> {
+  const keyword = req.query.keyword;
+  res.send(`Searching for ${keyword}`);
+});
+
+
+
 app.listen(PORT,()=>{
   console.log(`Server is running on http://localhost:${PORT}`)
-})
+});
